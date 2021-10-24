d00e93e4a8a98dfaf75269940f52ee6ea015a0f2
Poltergeist007 <77159690+girish-07@users.noreply.github.com>
2021-10-24 07:47:36 +0530

    A - NEW ADVENTURE
    Solution to the problem New Adventure

---
+++ TCDG2021/CDNEWADV.py
@@ -0,0 +1,7 @@
+testcases = int(input())
+for test in range (0, testcases):
+    m, n = map(int, input().split())
+    seta = set(map(str, input().split()))
+    setb = set(map(str, input().split()))
+    setb = setb.intersection(seta)
+    print(len(setb))
